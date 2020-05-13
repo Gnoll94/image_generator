6c0b8535923db04f80c7e20ff1b1408bff08f95e
Nolan <Gnoll94@gmail.com>
2020-05-12 22:46:13 -0400

    Adding canvas

--- src/App.js
+++ src/App.js
@@ -5,20 +5,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <canvas id="stockGraph" width="150" height="150">
+            current stock price: $3.15 + 0.15
+        </canvas>
     </div>
   );
 }
